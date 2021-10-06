538249ec0fda37a1252aa7f2fbb008ab27e0c6ae
kewablay <kojokewajunior123@gmail.com>
2021-10-06 14:27:37 +0000

    added new models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    transaction_id = models.CharField(max_length=200, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.id)
