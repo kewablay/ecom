2de8b32b5d07bb0e35959433342dca65a9df8cb8
kewablay <kojokewajunior123@gmail.com>
2021-10-08 14:47:49 +0000

    added changese to the sotre.html

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i< updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.productId
+        var action = this.dataset.action
+        console.log("productId:", productId, 'Action:', action)
+
+        console.log('USER',user)
+        if (user === "AnonymousUser"){
+            console.log('No logged in')
+        }else
+            console.log("user is logged in sending data")
+    })
+}
